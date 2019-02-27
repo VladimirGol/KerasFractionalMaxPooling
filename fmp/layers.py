ce15d7664f860848321ad9b95d867abf377d7e13
Головачев Владимир Владимирович <GolovachevV@s-avt.local>
2019-02-27 15:58:50 +0300

    Initial Commit

---
+++ fmp/layers.py
@@ -0,0 +1,44 @@
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Layer
+
+
+class FractionalMaxpool2D(Layer):
+    def __init__(self, pooling_ratio, pseudo_random=False, overlapping=False,
+                seed=0,data_format=None):
+        super(FractionalMaxpool2D, self).__init__()
+        self.pooling_ratio = pooling_ratio
+        self.pseudo_random = pseudo_random
+        self.overlapping = overlapping
+        self.seed = seed
+        self.data_format = data_format
+    def build(self, input_shape):
+        # Create a trainable weight variable for this layer.
+        # This kind of layer doesn't have any variable
+        pass
+    def call(self, x):
+        from tensorflow.nn import fractional_max_pool
+        return fractional_max_pool(x,pooling_ratio=self.pooling_ratio,
+                                      pseudo_random = self.pseudo_random, 
+                                      overlapping=self.overlapping,
+                                      seed = self.seed
+                                     )[0]  
+    def compute_output_shape(self, input_shape):
+        # return the output shape
+        from tensorflow.math import floor
+        
+        
+        if self.data_format == 'channels_first':
+            rows = input_shape[2]
+            cols = input_shape[3]
+        elif self.data_format == 'channels_last':
+            rows = input_shape[1]
+            cols = input_shape[2]
+        
+        rows = floor(rows / self.pooling_ratio[1])
+        cols = floor(cols / self.pooling_ratio[2])
+        
+        if self.data_format == 'channels_first':
+            return (input_shape[0], input_shape[1], rows, cols)
+        elif self.data_format == 'channels_last':
+            return (input_shape[0], rows, cols, input_shape[3])
+        
