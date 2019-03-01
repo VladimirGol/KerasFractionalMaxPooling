3f0757bbadffc08ab03befdeb8037cac8581077e
Головачев Владимир Владимирович <GolovachevV@s-avt.local>
2019-03-01 15:41:11 +0300

    Change layer name and pool_ratio argument

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KerasFractionalMaxPooling
 An implementation of FractionalMaxPooling Layer in Keras 
+Usage: Copy code from layers as is. Pooling ration should be a tuple of 2 floats
+between 1 and 2.
+Adding a layer to model: model.add(FractionalMaxPooling2D((1.414, 1.414)))
--- fmp/layers.py
+++ fmp/layers.py
@@ -1,23 +1,25 @@
+#KerasFractionalMaxPooling
+
 from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Layer
 
 
-class FractionalMaxpool2D(Layer):
+class FractionalMaxPooling2D(Layer):
     def __init__(self, pooling_ratio, pseudo_random=False, overlapping=False,
                 seed=0,data_format=None):
-        super(FractionalMaxpool2D, self).__init__()
+        super(FractionalMaxPooling2D, self).__init__()
         self.pooling_ratio = pooling_ratio
         self.pseudo_random = pseudo_random
         self.overlapping = overlapping
         self.seed = seed
         self.data_format = data_format
     def build(self, input_shape):
-        # Create a trainable weight variable for this layer.
-        # This kind of layer doesn't have any variable
+        
         pass
     def call(self, x):
         from tensorflow.nn import fractional_max_pool
-        return fractional_max_pool(x,pooling_ratio=self.pooling_ratio,
+        pr = [1., self.pooling_ratio[0], self.pooling_ratio[1], 1.]
+        return fractional_max_pool(x,pooling_ratio=pr,
                                       pseudo_random = self.pseudo_random, 
                                       overlapping=self.overlapping,
                                       seed = self.seed
@@ -34,11 +36,10 @@ class FractionalMaxpool2D(Layer):
             rows = input_shape[1]
             cols = input_shape[2]
         
-        rows = floor(rows / self.pooling_ratio[1])
-        cols = floor(cols / self.pooling_ratio[2])
+        rows = floor(rows / self.pooling_ratio[0])
+        cols = floor(cols / self.pooling_ratio[1])
         
         if self.data_format == 'channels_first':
             return (input_shape[0], input_shape[1], rows, cols)
         elif self.data_format == 'channels_last':
             return (input_shape[0], rows, cols, input_shape[3])
-        
